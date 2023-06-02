ff4613e2f9e9285d50196dfce372ac4c05cce6af
Theam-Dev <phearakroun@gmail.com>
2023-06-02 22:48:59 +0700

    Multi Select and Show Preview

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>Vue 3 Multiple select and show preview</h2>
+  <div style="position: relative;left: 30%;width: 40%;">
+    <vue-multi-image-upload @data-image="images" :max="4" :data-reset="component" :options="options" :image-size="imageSize"
+    :image-format="formatType" />
+  </div>
+ 
+  <button @click="component.clear = true">Clear</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { VueMultiImageUpload } from '@zakerxa/vue-multiple-image-upload';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VueMultiImageUpload
+  },
+  data() {
+    return {
+      component: {},
+      options: {},
+      imageSize: 2000000, // 2MB
+      formatType: ['image/jpeg', 'image/png', 'image/jpg']
+    }
+  },
+  methods: {
+    images(e) {
+      e.map(res => console.log(res))
+    },
+    clear() {
+      this.component.clear = true;
+    }
+  },
+  mounted() {
+    this.options.max = "Max";
+    this.options.ready = "Ready";
+    this.options.select = "Choosed";
   }
 }
 </script>
@@ -22,5 +47,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button{
+    width: 150px;
+    background-color: #41b883;
+    height: 40px;
+    border: 0px;
+    border-radius: 5px;
+    color: white;
 }
 </style>
